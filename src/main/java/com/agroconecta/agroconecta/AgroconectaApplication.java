e9cfe5be942ef684d5ea93a2c77106a320094461
BrandoSaenzRojas <brandosaenzrojas@hotmail.com>
2025-11-02 02:35:26 -0500

    feat(core): inicializa proyecto AgroConecta con configuración base de Spring Boot y dependencias esenciales

---
+++ src/main/java/com/agroconecta/agroconecta/AgroconectaApplication.java
@@ -0,0 +1,13 @@
+package com.agroconecta.agroconecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroconectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroconectaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agroconecta/agroconecta/AgroconectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroconecta.agroconecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroconectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
